736959bff426f944cc05683ef8bac34085774479
rtopalli95 <ragiptopalli@gmail.com>
2022-03-22 01:07:12 +0100

    RecSection

--- src/RecSection.js
+++ src/RecSection.js
@@ -1,10 +1,109 @@
 import React from 'react'
 import "./RecSection.css"
+import VideoCard from "./VideoCard"
 
 function RecSection() {
   return (
     <div className='recSection'>
       <h2>Recommended</h2>
+      <div className="recSection__videos">
+        <VideoCard
+          title="Yeah this is good Yeah this is good Yeah this is good Yeah this is good" 
+          views="16.3M Views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/yti/APfAmoFOLBLAac6V1uwbKWByeGSQBQp_-iD_vHBLwgKj2Q=s88-c-k-c0x00ffffff-no-rj-mo"
+          channel="Ragip Topalli"
+          image="https://scontent.fprn12-1.fna.fbcdn.net/v/t1.18169-9/13315347_534040826797943_5236951815616609466_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=HTcbdBHh6F8AX-B-ayp&_nc_ht=scontent.fprn12-1.fna&oh=00_AT-vnFwmQIxx5-8INhaDqvDUOXguJyvb-hVmFC1Qo0kB1g&oe=625EDD66"
+        />
+        <VideoCard
+          title="Yeah this is good"
+          views="16.3M Views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/yti/APfAmoFOLBLAac6V1uwbKWByeGSQBQp_-iD_vHBLwgKj2Q=s88-c-k-c0x00ffffff-no-rj-mo"
+          channel="Ragip Topalli"
+          image="https://scontent.fprn12-1.fna.fbcdn.net/v/t1.18169-9/13315347_534040826797943_5236951815616609466_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=HTcbdBHh6F8AX-B-ayp&_nc_ht=scontent.fprn12-1.fna&oh=00_AT-vnFwmQIxx5-8INhaDqvDUOXguJyvb-hVmFC1Qo0kB1g&oe=625EDD66"
+        />
+        <VideoCard
+          title="Yeah this is good"
+          views="16.3M Views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/yti/APfAmoFOLBLAac6V1uwbKWByeGSQBQp_-iD_vHBLwgKj2Q=s88-c-k-c0x00ffffff-no-rj-mo"
+          channel="Ragip Topalli"
+          image="https://scontent.fprn12-1.fna.fbcdn.net/v/t1.18169-9/13315347_534040826797943_5236951815616609466_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=HTcbdBHh6F8AX-B-ayp&_nc_ht=scontent.fprn12-1.fna&oh=00_AT-vnFwmQIxx5-8INhaDqvDUOXguJyvb-hVmFC1Qo0kB1g&oe=625EDD66"
+        />
+        <VideoCard
+          title="Yeah this is good"
+          views="16.3M Views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/yti/APfAmoFOLBLAac6V1uwbKWByeGSQBQp_-iD_vHBLwgKj2Q=s88-c-k-c0x00ffffff-no-rj-mo"
+          channel="Ragip Topalli"
+          image="https://scontent.fprn12-1.fna.fbcdn.net/v/t1.18169-9/13315347_534040826797943_5236951815616609466_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=HTcbdBHh6F8AX-B-ayp&_nc_ht=scontent.fprn12-1.fna&oh=00_AT-vnFwmQIxx5-8INhaDqvDUOXguJyvb-hVmFC1Qo0kB1g&oe=625EDD66"
+        />
+        <VideoCard
+          title="Yeah this is good"
+          views="16.3M Views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/yti/APfAmoFOLBLAac6V1uwbKWByeGSQBQp_-iD_vHBLwgKj2Q=s88-c-k-c0x00ffffff-no-rj-mo"
+          channel="Ragip Topalli"
+          image="https://scontent.fprn12-1.fna.fbcdn.net/v/t1.18169-9/13315347_534040826797943_5236951815616609466_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=HTcbdBHh6F8AX-B-ayp&_nc_ht=scontent.fprn12-1.fna&oh=00_AT-vnFwmQIxx5-8INhaDqvDUOXguJyvb-hVmFC1Qo0kB1g&oe=625EDD66"
+        />
+        <VideoCard
+          title="Yeah this is good"
+          views="16.3M Views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/yti/APfAmoFOLBLAac6V1uwbKWByeGSQBQp_-iD_vHBLwgKj2Q=s88-c-k-c0x00ffffff-no-rj-mo"
+          channel="Ragip Topalli"
+          image="https://scontent.fprn12-1.fna.fbcdn.net/v/t1.18169-9/13315347_534040826797943_5236951815616609466_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=HTcbdBHh6F8AX-B-ayp&_nc_ht=scontent.fprn12-1.fna&oh=00_AT-vnFwmQIxx5-8INhaDqvDUOXguJyvb-hVmFC1Qo0kB1g&oe=625EDD66"
+        />
+        <VideoCard
+          title="Yeah this is good"
+          views="16.3M Views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/yti/APfAmoFOLBLAac6V1uwbKWByeGSQBQp_-iD_vHBLwgKj2Q=s88-c-k-c0x00ffffff-no-rj-mo"
+          channel="Ragip Topalli"
+          image="https://scontent.fprn12-1.fna.fbcdn.net/v/t1.18169-9/13315347_534040826797943_5236951815616609466_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=HTcbdBHh6F8AX-B-ayp&_nc_ht=scontent.fprn12-1.fna&oh=00_AT-vnFwmQIxx5-8INhaDqvDUOXguJyvb-hVmFC1Qo0kB1g&oe=625EDD66"
+        />
+        <VideoCard
+          title="Yeah this is good"
+          views="16.3M Views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/yti/APfAmoFOLBLAac6V1uwbKWByeGSQBQp_-iD_vHBLwgKj2Q=s88-c-k-c0x00ffffff-no-rj-mo"
+          channel="Ragip Topalli"
+          image="https://scontent.fprn12-1.fna.fbcdn.net/v/t1.18169-9/13315347_534040826797943_5236951815616609466_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=HTcbdBHh6F8AX-B-ayp&_nc_ht=scontent.fprn12-1.fna&oh=00_AT-vnFwmQIxx5-8INhaDqvDUOXguJyvb-hVmFC1Qo0kB1g&oe=625EDD66"
+        />
+        <VideoCard
+          title="Yeah this is good"
+          views="16.3M Views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/yti/APfAmoFOLBLAac6V1uwbKWByeGSQBQp_-iD_vHBLwgKj2Q=s88-c-k-c0x00ffffff-no-rj-mo"
+          channel="Ragip Topalli"
+          image="https://scontent.fprn12-1.fna.fbcdn.net/v/t1.18169-9/13315347_534040826797943_5236951815616609466_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=HTcbdBHh6F8AX-B-ayp&_nc_ht=scontent.fprn12-1.fna&oh=00_AT-vnFwmQIxx5-8INhaDqvDUOXguJyvb-hVmFC1Qo0kB1g&oe=625EDD66"
+        />
+        <VideoCard
+          title="Yeah this is good"
+          views="16.3M Views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/yti/APfAmoFOLBLAac6V1uwbKWByeGSQBQp_-iD_vHBLwgKj2Q=s88-c-k-c0x00ffffff-no-rj-mo"
+          channel="Ragip Topalli"
+          image="https://scontent.fprn12-1.fna.fbcdn.net/v/t1.18169-9/13315347_534040826797943_5236951815616609466_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=HTcbdBHh6F8AX-B-ayp&_nc_ht=scontent.fprn12-1.fna&oh=00_AT-vnFwmQIxx5-8INhaDqvDUOXguJyvb-hVmFC1Qo0kB1g&oe=625EDD66"
+        />
+        <VideoCard
+          title="Yeah this is good"
+          views="16.3M Views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/yti/APfAmoFOLBLAac6V1uwbKWByeGSQBQp_-iD_vHBLwgKj2Q=s88-c-k-c0x00ffffff-no-rj-mo"
+          channel="Ragip Topalli"
+          image="https://scontent.fprn12-1.fna.fbcdn.net/v/t1.18169-9/13315347_534040826797943_5236951815616609466_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=HTcbdBHh6F8AX-B-ayp&_nc_ht=scontent.fprn12-1.fna&oh=00_AT-vnFwmQIxx5-8INhaDqvDUOXguJyvb-hVmFC1Qo0kB1g&oe=625EDD66"
+        />
+        <VideoCard
+          title="Yeah this is good"
+          views="16.3M Views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/yti/APfAmoFOLBLAac6V1uwbKWByeGSQBQp_-iD_vHBLwgKj2Q=s88-c-k-c0x00ffffff-no-rj-mo"
+          channel="Ragip Topalli"
+          image="https://scontent.fprn12-1.fna.fbcdn.net/v/t1.18169-9/13315347_534040826797943_5236951815616609466_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=HTcbdBHh6F8AX-B-ayp&_nc_ht=scontent.fprn12-1.fna&oh=00_AT-vnFwmQIxx5-8INhaDqvDUOXguJyvb-hVmFC1Qo0kB1g&oe=625EDD66"
+        />
+      </div>
     </div>
   )
 }
